7e5ad419837a0e12d0c0a1b211be80277788990c
Anuj Singh Kushwaha <anujsk1208@gmail.com>
2023-03-07 13:33:27 +0530

    initial commit

---
+++ app/src/main/java/com/example/nitcmessmanager/MainActivity.java
@@ -0,0 +1,67 @@
+package com.example.nitcmessmanager;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+//import android.widget.*;
+
+public class MainActivity extends AppCompatActivity {
+    private TextView textView;
+    //private EditText editText;
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+       // super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button button1 = findViewById(R.id.button1);
+        Button button2 = findViewById(R.id.button2);
+        Button button3 = findViewById(R.id.button3);
+        Button button4 = findViewById(R.id.button4);
+        textView = findViewById(R.id.textView);
+        //editText = findViewById(R.id.editText);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openActivityRegistration();
+            }
+        });
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openActivityLogin();
+            }
+        });
+       /* button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openActivityAdminLogin();
+            }
+        });*/
+        button4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "Hi USER!!!", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+    public void openActivityRegistration() {
+        Intent I=new Intent(this, RegistrationActivity.class);
+        startActivity(I);
+    }
+    public void openActivityLogin() {
+        Intent I=new Intent(this, LoginActivity.class);
+        startActivity(I);
+    }
+    /*public void openActivityAdminLogin() {
+        Intent I=new Intent(this, AdminLoginActivity.class);
+        startActivity(I);
+    }*/
+}
